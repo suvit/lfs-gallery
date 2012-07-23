57bbc4009ce3a1a2cee694d4a5265371cd6b15a8
Victor Safronovich <vsafronovich@gmail.com>
2012-07-23 16:04:16 +0500

    fixed path to views do urls more restfull a Please enter the commit message for your changes. Lines starting

--- lfs_gallery/urls.py
+++ lfs_gallery/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('pwgallery.views',
+urlpatterns = patterns('lfs_gallery.views',
 
-    url(r'^manage/add_gallery/$', "add_gallery", name="add_gallery"),
-    url(r'^manage/del_gallery/(?P<id>\d*)/$', "del_gallery", name="del_gallery"),
+    url(r'^manage/gallery/add/$', "add_gallery", name="add_gallery"),
+    url(r'^manage/gallery/(?P<id>\d*)/delete/$', "del_gallery",
+        name="del_gallery"),
 
-    url(r'^manage/add_gallery_item/$', "add_gallery_item", name="add_gallery_item"),
-    url(r'^manage/del_gallery_item/(?P<id>\d*)/$', "del_gallery_item", name="del_gallery_item"),
+    url(r'^manage/gallery/item/add/$', "add_gallery_item",
+        name="add_gallery_item"),
+    url(r'^manage/gallery/item/(?P<id>\d*)/delete/$', "del_gallery_item",
+        name="del_gallery_item"),
 
-    url(r'^manage/manage_gallery_items/$', "manage_gallery_items", name="manage_gallery_items"),
-    url(r'^manage/manage_gallery_item/(?P<id>\d*)/$', "manage_gallery_item", name="manage_gallery_item"),
+    url(r'^manage/gallery/$', "manage_gallery_items",
+        name="manage_gallery_items"),
+    url(r'^manage/gallery/item/(?P<id>\d*)/$', "manage_gallery_item",
+        name="manage_gallery_item"),
 )
