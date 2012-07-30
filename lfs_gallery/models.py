dc27b16f7c7d5f744b4e3481e5f45905126c32c3
Victor Safronovich <vsafronovich@gmail.com>
2012-07-30 15:20:16 +0500

    added translations

--- lfs_gallery/models.py
+++ lfs_gallery/models.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from django.core.cache import cache
 from django.db import models
-from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
 
 CACHE_PREFIX = settings.CACHE_MIDDLEWARE_KEY_PREFIX
 
 
 class Gallery(models.Model):
 
-    title = models.CharField(verbose_name=u"Название",
+    title = models.CharField(verbose_name=_(u'Title'),
                              max_length=100,
                              blank=True, unique=True)
 
@@ -27,17 +29,18 @@ class Gallery(models.Model):
 
 class GalleryItem(models.Model):
 
-    gallery = models.ForeignKey(Gallery, verbose_name=u"Галерея")
+    gallery = models.ForeignKey(Gallery,
+                                verbose_name=_(u'Gallery'))
 
-    title = models.CharField(verbose_name=u"Название", max_length=100)
+    title = models.CharField(verbose_name=_(u"Title"), max_length=100)
 
-    link = models.CharField(verbose_name=u"Ссылка", blank=True, max_length=100)
+    link = models.CharField(verbose_name=_(u"Link"), blank=True, max_length=100)
 
-    active = models.BooleanField(verbose_name=u"Активно", default=False)
+    active = models.BooleanField(verbose_name=_(u"Active"), default=False)
 
-    position = models.IntegerField(verbose_name=u"Позиция", default=999)
+    position = models.IntegerField(verbose_name=_(u"Position"), default=999)
 
-    image = models.ImageField(verbose_name=u"Картинка",
+    image = models.ImageField(verbose_name=_(u"Image"),
                               upload_to='gallery')
 
     def __unicode__(self):
